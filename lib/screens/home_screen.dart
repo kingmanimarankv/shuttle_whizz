1291d1f093380690aeeab5515575c1eb56bfd0d5
Indhumathi Elumalai <indhumathi.e04@gmail.com>
2023-02-12 04:09:22 -0500

    Remove member import

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shuttle_whizz/screens/widgets/hero_section.dart';
 import 'package:shuttle_whizz/screens/widgets/help_section.dart';
-import 'package:shuttle_whizz/screens/widgets/member.dart';
+
 import 'package:shuttle_whizz/screens/widgets/play_it_forward_section.dart';
 import 'package:shuttle_whizz/screens/widgets/preference_section.dart';
 
